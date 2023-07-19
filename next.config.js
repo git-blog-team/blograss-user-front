38521fa11802571f1e6ec730d2bc3541b10ad98f
nostrss <jintagi@gmail.com>
2023-07-20 01:52:32 +0900

    add zustand

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    trailingSlash: true,
+};
 
-module.exports = nextConfig
+// eslint-disable-next-line no-undef
+module.exports = nextConfig;
