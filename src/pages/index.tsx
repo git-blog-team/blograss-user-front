f854aa7f6fe14dd4d7aa78e5d01a2b658c4ea64d
nostrss <jintagi@gmail.com>
2023-08-17 23:52:40 +0900

    ssr test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,23 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import axios from '@/api/middlewares';
 import ListPageHeader from '@/components/ListPageHeader';
 import { BLOGRASS_AUTH_GET_TOKEN } from '@/constants/api';
 import styled from '@emotion/styled';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
 
 export default function Home() {
-    const { query, push } = useRouter();
+    // console.log(data);
+    // const { query, push } = useRouter();
 
-    useEffect(() => {
-        if (!query.code) return;
+    // useEffect(() => {
+    //     if (!query.code) return;
 
-        const getGithubAuthToken = async () => {
-            return await axios.get(`${BLOGRASS_AUTH_GET_TOKEN}${query.code}`);
-        };
-        getGithubAuthToken().then(() => {
-            push('/');
-        });
-    }, [query.code]);
+    //     const getGithubAuthToken = async () => {
+    //         return await axios.get(`${BLOGRASS_AUTH_GET_TOKEN}${query.code}`);
+    //     };
+    //     getGithubAuthToken().then(() => {
+    //         push('/');
+    //     });
+    // }, [query.code]);
 
     return (
         <StyledMain>
@@ -33,3 +33,51 @@ const StyledMain = styled.section`
     width: 100%;
     background-color: #fff;
 `;
+
+// interface Props {
+//     req: any;
+//     query: string;
+// }
+
+// export async function getServerSideProps({ req, query }: Props) {
+//     console.log('params😏', req.cookies, query);
+//     // Fetch data from external API
+//     // const res = await fetch(`https://.../data`)
+//     // const data = await res.json()
+
+//     // Pass data to the page via props
+//     return {
+//         props: {
+//             data: {},
+//         },
+//     };
+// }
+
+interface Props {
+    req: any;
+    query: any;
+}
+
+export const getServerSideProps = async ({
+    req: { cookies },
+    query,
+}: Props) => {
+    console.log(cookies, query);
+    if (!query.code)
+        return {
+            props: {
+                data: {},
+            },
+        };
+    await axios.get(`${BLOGRASS_AUTH_GET_TOKEN}${query.code}`);
+
+    return {
+        redirect: {
+            destination: '/',
+            permanent: false,
+        },
+        props: {
+            data: {},
+        },
+    };
+};
