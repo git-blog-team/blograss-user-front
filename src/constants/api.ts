c53bef30a66e1f8e2965f214721f326a84005066
nostrss <jintagi@gmail.com>
2023-08-19 17:14:42 +0900

    get user data

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,3 +1,4 @@
 export const BLOGRASS_AUTH_GET_TOKEN = '/auth?code=';
 export const BLOGRASS_GET_USER_DATA = '/auth/user';
 export const BLOGRASS_USER_LOGOUT = '/auth/logout';
+export const BLOGRASS_TOKEN_REPUBLISH = '/auth/tokenrepubilsh';
