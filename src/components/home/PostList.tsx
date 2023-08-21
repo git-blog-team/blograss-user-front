bf986b7c97b05c80d23100dd8c95e38f7390f27e
nostrss <jintagi@gmail.com>
2023-08-21 19:22:03 +0900

    add link from list to detail

--- src/components/home/PostList.tsx
+++ src/components/home/PostList.tsx
@@ -1,6 +1,8 @@
 import { postAPI } from '@/api/postAPI';
+import { BLOGRASS_BASE_URL } from '@/constants/api';
 import { PostItem } from '@/types/postType';
 import { useQuery } from '@tanstack/react-query';
+import Link from 'next/link';
 
 export default function PostList() {
     const { data } = useQuery({
@@ -13,10 +15,14 @@ export default function PostList() {
             <ol>
                 {data?.data.result[0].content.map((postItem: PostItem) => (
                     <li key={postItem.postId}>
-                        <article>
-                            <h2>title : {postItem.title}</h2>
-                            <p>content : {postItem.content}</p>
-                        </article>
+                        <Link
+                            href={`${BLOGRASS_BASE_URL}/post/${postItem.postId}`}
+                        >
+                            <article>
+                                <h2>title : {postItem.title}</h2>
+                                <p>content : {postItem.content}</p>
+                            </article>
+                        </Link>
                     </li>
                 ))}
             </ol>
