8b74ba4b3eed2464ff644ef2f57cc62fdf53be8e
nostrss <jintagi@gmail.com>
2023-08-25 15:36:42 +0900

    add feature image upload

--- src/api/imageAPI.ts
+++ src/api/imageAPI.ts
@@ -7,32 +7,20 @@ import { HookCallback } from '@toast-ui/editor/types/editor';
 
 export const imageAPI = {
     postUploadImage: async (image: File | Blob, callback: HookCallback) => {
-        const imageFile = new FormData();
+        const formData = new FormData();
+        formData.append('file', image);
 
-        const file = new File([image], image.name);
-
-        // imageFile.append
-
-        imageFile.append('image', file);
-
-        /**
-         * 이미지 정보가 있으나
-         * FormData 정보가 생성이 안됨
-         */
-        console.log('file', file);
-        console.log('imageFile', imageFile);
-
-        await axios
-            .post(BLOGRASS_IMAGE_UPLOAD, {
-                imageFile,
-            })
-            .then((res) => {
-                console.log(res);
-                const result = `${BLOGRASS_IMAGE_BUCKET_URL}/${res.data.result[0]}`;
-                callback(result, 'alt_text');
-            })
-            .catch((error) => {
-                console.error(error);
+        try {
+            const result = await axios({
+                method: 'post',
+                url: BLOGRASS_IMAGE_UPLOAD,
+                data: formData,
             });
+            const imageUrl =
+                await `${BLOGRASS_IMAGE_BUCKET_URL}/${result.data.result[0]}`;
+            callback(imageUrl, 'alt_text');
+        } catch (error) {
+            console.error(error);
+        }
     },
 };
