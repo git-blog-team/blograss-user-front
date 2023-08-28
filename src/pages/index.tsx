d5fb1cc7b04c7fbe0d774d213fed88b8fafd56da
nostrss <jintagi@gmail.com>
2023-08-28 10:41:39 +0900

    sign in/up fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,63 +1,23 @@
 import ListPageHeader from '@/components/home/ListPageHeader';
 import styled from '@emotion/styled';
-import { useRouter } from 'next/router';
-import { authAPI } from '@/api/authAPI';
-import { useQuery } from '@tanstack/react-query';
-import GridLoader from '@/components/common/GridLoader';
-import theme from '@/styles/theme';
-import { ColumnCenterCenter } from '@/styles/flexModules';
 import PostList from '@/components/home/PostList';
+import Axios from 'axios';
+import {
+    BLOGRASS_API_BASE_URL,
+    BLOGRASS_AUTH_GET_TOKEN,
+} from '@/constants/api';
+import { GetServerSideProps } from 'next';
 
-interface IProps {
-    query: {
-        code?: string;
-    };
-}
-
-export default function Home({ query }: IProps) {
-    const { push } = useRouter();
-    const { isFetching, error } = useQuery({
-        queryKey: ['getAuthToken'],
-        queryFn: async () => {
-            const data = await authAPI.getAuthToken(query.code).finally(() => {
-                push('/');
-            });
-            return data;
-        },
-        enabled: !!query.code,
-    });
-
-    if (error) {
-        console.error(error);
-    }
-
+export default function Home() {
     return (
         <>
-            {isFetching ? (
-                <StyledWrapperLoader>
-                    <GridLoader
-                        size={100}
-                        color={theme.colors.point_green}
-                        speedMultiplier={0.8}
-                    />
-                </StyledWrapperLoader>
-            ) : (
-                <StyledSection>
-                    <ListPageHeader />
-                    <PostList />
-                </StyledSection>
-            )}
+            <StyledSection>
+                <ListPageHeader />
+                <PostList />
+            </StyledSection>
         </>
     );
 }
-
-const StyledWrapperLoader = styled.section`
-    ${ColumnCenterCenter}
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-    height: 100vh;
-`;
 
 const StyledSection = styled.section`
     display: flex;
@@ -66,10 +26,25 @@ const StyledSection = styled.section`
     background-color: #fff;
 `;
 
-export const getServerSideProps = async ({ query }: IProps) => {
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const cookieOption = `Secure ; Max-Age=${60 * 60 * 24 * 180}`;
+    if (context.query.code) {
+        const response = await Axios.get(
+            `${BLOGRASS_API_BASE_URL}${BLOGRASS_AUTH_GET_TOKEN}${context.query.code}`,
+        );
+        context.res.setHeader('set-Cookie', [
+            `accessToken=${response.data.result[0].accessToken}; ${cookieOption}`,
+            `refreshToken=${response.data.result[0].refreshToken}; ${cookieOption}`,
+        ]);
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false,
+            },
+            props: {},
+        };
+    }
     return {
-        props: {
-            query,
-        },
+        props: {},
     };
 };
