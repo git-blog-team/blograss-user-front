f50e97b862de083a881d4150ec493daaeafef16f
nostrss <jintagi@gmail.com>
2023-08-28 10:49:10 +0900

    refactoring

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,8 @@
 import ListPageHeader from '@/components/home/ListPageHeader';
 import styled from '@emotion/styled';
 import PostList from '@/components/home/PostList';
-import Axios from 'axios';
-import {
-    BLOGRASS_API_BASE_URL,
-    BLOGRASS_AUTH_GET_TOKEN,
-} from '@/constants/api';
 import { GetServerSideProps } from 'next';
+import { authAPI } from '@/api/authAPI';
 
 export default function Home() {
     return (
@@ -29,9 +25,7 @@ const StyledSection = styled.section`
 export const getServerSideProps: GetServerSideProps = async (context) => {
     const cookieOption = `Secure ; Max-Age=${60 * 60 * 24 * 180}`;
     if (context.query.code) {
-        const response = await Axios.get(
-            `${BLOGRASS_API_BASE_URL}${BLOGRASS_AUTH_GET_TOKEN}${context.query.code}`,
-        );
+        const response = await authAPI.getAuthToken(context.query.code);
         context.res.setHeader('set-Cookie', [
             `accessToken=${response.data.result[0].accessToken}; ${cookieOption}`,
             `refreshToken=${response.data.result[0].refreshToken}; ${cookieOption}`,
